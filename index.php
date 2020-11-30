b0c965c0643a562807f87c40c1359a47a0a1c5f8
Tiagohcode <74985078+Tiagohcode@users.noreply.github.com>
2020-11-30 20:03:21 -0300

    traduzi o texto

--- index.php
+++ index.php
@@ -1,24 +1,5 @@
 <?php
 
-//echo "Meu primeiro código PHP! <b>Hello World</b>";
-
-/*$nome = "Tiago";
-$sobrenome = "Queiroz";
-$numero = 1;
-
-echo "<b>" . $nome . "</b>" . " " . $sobrenome;
-
-$data = date("Y"); //2020
-
-$termino = 1900;
-
-for ($i = $data; $i >= $termino; $i--) { //true ou false
-	echo $i . "<br>";
-}*/
-
-for ($i = 1920; $i <= 2020; $i++) {
-	echo $i . "<br>";
-}
-
+echo "Olá Mundo ";
 
 ?>
